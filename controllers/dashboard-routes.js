8e8e6b5d75304772eda8c463b2053ad8fa2ab129
gio2nice <106710271+gio2nice@users.noreply.github.com>
2022-09-12 20:28:23 -0700

    route

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -2,3 +2,18 @@ const router = require('express').Router();
 const sequelize = require('../config/connection');
 
 const { Post, User, Comment } = require('../models');
+const wAuth = require('../utils')
+
+router.get('/', wAuth, (req, res) => {
+    Post.findAll({
+        where: {
+            user_id: req.session.user_id
+        },
+        attributes: [
+            'id',
+            'post_text',
+            'title',
+            'created_at',
+        ]
+    })
+})
